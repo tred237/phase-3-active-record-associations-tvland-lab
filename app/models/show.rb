49dce769f1a3f0c56f818af296cbebbab13680c7
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-06 22:13:55 -0700

    commit tree 2683e0376d5951014bd32c06dee473a14404a8fd

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map{|i| "#{i.first_name} #{i.last_name}"}
+    end
 end
